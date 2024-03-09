804f9c5cf1881e28fa23327b2027c9df111caac8
techy-savant <ogbanufavour@gmail.com>
2024-03-09 09:45:21 +0100

    added context store and logic

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,16 +1,20 @@
-import mealsImg from "../../assets/meals.jpg";
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
-    <>
-      <header className="header">
-        <h1>YumYum</h1>
-        <button>Cart</button>
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Yum-Meals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div className="main-image">
-        <img src={mealsImg} alt="table of sweet food" />
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
-    </>
+    </Fragment>
   );
 };
 
